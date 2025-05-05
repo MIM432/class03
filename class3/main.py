7eaf06e981411826fc78254fc23e4efa9a9b60bf
MIM432 <m.ibrahim135902@gmail.com>
2025-05-05 16:58:54 +0500

    Add files via upload

---
+++ class3/main.py
@@ -0,0 +1,43 @@
+# Slicingllo"))
+# print("substring to search for     =","Hello")
+# print("starting index of substring = ",len("Hello"))
+# print("length of substring         = ",len(my_string))
+# print("Second  index  = ",my_string(("hello"),len("Hello")))
+# print(dir(str))
+# print([mehod for mehod in dir(str) if "__" not in mehod])
+# print(my_string_0.upper())
+# print(my_string_10.lower())
+# print(my_string_0.upper())
+# print(my_string_10.lower())
+my_string_1 : str ="Hello"
+my_string_2 : str ="World"
+# print(my_string_3[-1])
+# print(len(my_string_3))
+# print(my_string_1 + my_string_2)
+my_string_3:str =""
+my_string_3 = my_string_3.join(["Hello", "World"])
+# print(my_string_3)
+# print(my_string_3[0:11:2])
+my_string_3.replace("Hello", "Hi ")
+# print(my_string_3)
+# my_string_0:str ="Anas"
+# my_string_10:str ="Umar"
+my_string:str = "Hello, World! Hello, Pakistan"
+starting_index =my_string.find("Hello")
+starting_index1 = starting_index + len("Hello")
+# print(my_string[starting_index1:])
+# print(my_string[starting_index1:].find("He
+# print(my_string_3.split())
+# print(my_string_3.split("l"))
+# my_strings:str = '''My name is %s and I am %d years old'''%()
+# a:str ="Anas"
+# b:str ="Umer"
+# c:str =a +""
+# print(c)
+# print(a)
+a= "hello"
+b= "hello"
+# print(a is b)
+a= "this is very long string"
+c= "this is very long string"
+print(a is c)
